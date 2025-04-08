ab27fd22edfa887b24082057000d90f1305eced6
Paul Stone <paul@paulstone.net>
2025-04-08 22:28:44 +0100

    When update from Craft v4 to v5, this migration errored. Moving the table clearing before adding the fk fixes it.

--- src/migrations/m240804_214123_converted_blocktype_to_entrytype.php
+++ src/migrations/m240804_214123_converted_blocktype_to_entrytype.php
@@ -15,6 +15,16 @@ class m240804_214123_converted_blocktype_to_entrytype extends Migration
      */
     public function safeUp(): bool
     {
+
+        // When updating from Craft v4 to v5, this migration errored. 
+        // Moving the table clearing before adding the fk fixes it.
+        
+        // Unfortunately there's not a way to migrate previous block configs over
+        // to the new entry types that Craft 5 uses for matrix fields. This means
+        // we need to delete any existing configs so that they can be recreated
+        // when the matrix field settings page is next visited by the user.
+        $this->delete('matrixfieldpreview_blocktypes_config');
+
         // Update the FK field to point to the entrytypes table
         $this->addForeignKey(
             $this->db->getForeignKeyName('{{%matrixfieldpreview_blocktypes_config}}', 'blockTypeId'),
@@ -26,12 +36,7 @@ class m240804_214123_converted_blocktype_to_entrytype extends Migration
             'CASCADE'
         );
 
-        // Unfortunately there's not a way to migrate previous block configs over
-        // to the new entry types that Craft 5 uses for matrix fields. This means
-        // we need to delete any existing configs so that they can be recreated
-        // when the matrix field settings page is next visited by the user.
-        $this->delete('matrixfieldpreview_blocktypes_config');
-
+    
         return true;
     }
 
